9447cbd7632e5acb58cc64aaf2976a288b3c68ac
jerry914 <joyce91400@hotmail.com>
2022-12-10 00:48:23 +0800

    delete helper mistake

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -19,9 +19,6 @@ require_app
 
 TOPIC_ID = 'xjPR4hlkBGA'
 VIEW_ID = 'JJSIg8xwKkQ'
-key_path = File.expand_path('../../config/secrets.yml', __dir__)
-CONFIG = YAML.safe_load(File.read(key_path))
-# UNSPLAH_TOKEN = CONFIG['UNSPLASH_SECRETS_KEY']
 UNSPLAH_TOKEN = LightofDay::App.config.UNSPLASH_SECRETS_KEY
 output_path = File.expand_path('../fixtures/unsplash_results.yml', __dir__)
 CORRECT = YAML.unsafe_load(File.read(output_path))
