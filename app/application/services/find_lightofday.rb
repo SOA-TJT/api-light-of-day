d73785cf1fab77235c338b91286654ee2a765084
Hsuan <lala092325@gmail.com>
2022-12-14 22:28:39 +0800

    test

--- app/application/services/find_lightofday.rb
+++ app/application/services/find_lightofday.rb
@@ -10,10 +10,24 @@ module LightofDay
       include Dry::Transaction
 
       step :remote_lightofday
+      
 
       private
 
       DB_ERR = 'Cannot access database'
+      
+      def request_cloning_worker(input)
+        return Success(input) if input[:gitrepo].exists_locally?
+
+        Messaging::Queue
+          .new(App.config.CLONE_QUEUE_URL, App.config)
+          .send(Representer::Project.new(input[:project]).to_json)
+
+        Failure(Response::ApiResult.new(status: :processing, message: PROCESSING_MSG))
+      rescue StandardError => e
+        print_error(e)
+        Failure(Response::ApiResult.new(status: :internal_error, message: CLONE_ERR))
+      end
 
       def remote_lightofday(input)
         lightofday = LightofDay::Unsplash::ViewMapper
