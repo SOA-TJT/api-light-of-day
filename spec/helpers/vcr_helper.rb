d0ba1676137d57dd44b0d98f0489bb28bda07531
Hsuan <lala092325@gmail.com>
2022-12-14 21:51:57 +0800

    VCR ignore calls to SQS

--- spec/helpers/vcr_helper.rb
+++ spec/helpers/vcr_helper.rb
@@ -23,6 +23,8 @@ module VcrHelper
     VCR.configure do |c|
       c.cassette_library_dir = CASSETTES_FOLDER
       c.hook_into :webmock
+      vcr_config.ignore_hosts 'sqs.us-east-1.amazonaws.com'
+      vcr_config.ignore_hosts 'sqs.ap-northeast-1.amazonaws.com'
     end
   end
 
