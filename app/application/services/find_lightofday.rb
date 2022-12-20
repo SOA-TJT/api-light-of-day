38a56aad3435ca097d07f9777708db641100bdad
Hsuan <lala092325@gmail.com>
2022-12-21 00:50:46 +0800

    restart random view

--- app/application/services/find_lightofday.rb
+++ app/application/services/find_lightofday.rb
@@ -7,38 +7,38 @@ module LightofDay
   module Service
     # Retrieves array of all listed project entities
     class FindLightofDay
-      include Dry::Transaction
+      include Dry::Monads::Result::Mixin
 
-      step :request_random_lightofday_worker
-      step :remote_lightofday
+      # step :request_random_lightofday_worker
+      # step :remote_lightofday
 
-      private
+      # private
 
-      FIND_ERR = 'Could not find this lightofday'
-      PROCESSING_MSG = 'Processing the summary request'
+      # FIND_ERR = 'Could not find this lightofday'
+      # PROCESSING_MSG = 'Processing the summary request'
       DB_ERR = 'Cannot access database'
 
-      def request_random_lightofday_worker(input)
-        # return Success(input) if input.exists_locally? # need to modify
-        a = { 'topic_id' => input }.to_json
-        puts a
+      # def request_random_lightofday_worker(input)
+      #   # return Success(input) if input.exists_locally? # need to modify
+      #   a = { 'topic_id' => input }.to_json
+      #   puts a
 
-        Messaging::Queue
-          .new(App.config.FAVORITE_QUEUE_URL, App.config)
-          .send({ 'topic_id' => input }.to_json)
+      #   Messaging::Queue
+      #     .new(App.config.FAVORITE_QUEUE_URL, App.config)
+      #     .send({ 'topic_id' => input }.to_json)
 
-        Failure(Response::ApiResult.new(status: :processing, message: PROCESSING_MSG))
-      rescue StandardError
-        # print_error(e)
-        Failure(Response::ApiResult.new(status: :internal_error, message: FIND_ERR))
-      end
+      #   Failure(Response::ApiResult.new(status: :processing, message: PROCESSING_MSG))
+      # rescue StandardError
+      #   # print_error(e)
+      #   Failure(Response::ApiResult.new(status: :internal_error, message: FIND_ERR))
+      # end
 
-      def remote_lightofday(input)
-        call_lightofday = LightofDay::Unsplash::ViewMapper
+      def call(input)
+        lightofday = LightofDay::Unsplash::ViewMapper
                           .new(App.config.UNSPLASH_SECRETS_KEY,
                                input).find_a_photo
 
-        lightofday = Response::FavoriteList.new(call_lightofday)
+        # lightofday = Response::FavoriteList.new(call_lightofday)
         Success(Response::ApiResult.new(status: :ok, message: lightofday))
       rescue StandardError
         # App.logger.error "Could not find..."
