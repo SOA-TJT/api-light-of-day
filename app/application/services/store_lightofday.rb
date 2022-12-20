37d747d6bbe874a79203e0b8d503589c014437c2
Hsuan <lala092325@gmail.com>
2022-12-21 00:50:37 +0800

    restart random view

--- app/application/services/store_lightofday.rb
+++ app/application/services/store_lightofday.rb
@@ -14,6 +14,21 @@ module LightofDay
 
       DB_ERR = 'Cannot access database'
 
+      def request_random_lightofday_worker(input)
+        return Success(input) if input.exists_locally? # need to modify
+        a = { 'topic_id' => input }.to_json
+        puts a
+
+        Messaging::Queue
+          .new(App.config.FAVORITE_QUEUE_URL, App.config)
+          .send({ 'topic_id' => input }.to_json)
+
+        Failure(Response::ApiResult.new(status: :processing, message: PROCESSING_MSG))
+      rescue StandardError
+        # print_error(e)
+        Failure(Response::ApiResult.new(status: :internal_error, message: FIND_ERR))
+      end
+      
       def store_lightofday(input)
         puts input
         lightofday = Repository::For.entity(input).create(input)
